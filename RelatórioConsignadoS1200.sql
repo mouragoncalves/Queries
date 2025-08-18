247b24de4fb8462a28f8e833e309799debff22f1
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2025-08-18 20:38:30 -0300

    ok

--- RelatórioConsignadoS1200.sql
+++ RelatórioConsignadoS1200.sql
@@ -1,17 +1,23 @@
-DECLARE @MainEntityCode VARCHAR(20) = '', @EntitiesCode VARCHAR(MAX) = '104122,101111', @RelatedYear INT = 2025, @RelatedMonth INT = 7;
+DECLARE @MainEntityCode VARCHAR(20) = '101122', @EntitiesCode VARCHAR(MAX) = '101122,101223,101225,101226,102122,103122,104122,105122,106122,107122,112122,113122', @RelatedYear INT = 2025, @RelatedMonth INT = 7;
 
 WITH
     EventsData
     AS
     (
-        SELECT e.Id EventId, e.RelatedYear, e.RelatedMonth, e.EntityCode, CONVERT(XML, c.Content) ContentXML
+        SELECT e.Id EventId, e.RelatedYear, e.RelatedMonth, e.EntityCode, e.BusinessKey, CONVERT(XML, c.Content) ContentXML
         FROM Event e
             INNER JOIN XMLContent c ON c.ReferenceId = e.Id AND c.ContentReferenceEnum = 0 AND CHARINDEX('<descFolha>', c.Content) > 0
         WHERE e.EventTypeEnum = 8
-            -- AND e.EntityCode IN (SELECT CAST(value AS varchar) FROM STRING_SPLIT(@EntitiesCode, ','))
+            AND e.EntityCode IN (SELECT CAST(value AS varchar) FROM STRING_SPLIT(@EntitiesCode, ','))
             AND e.RelatedYear = @RelatedYear
             AND e.RelatedMonth = @RelatedMonth
             AND e.EventStatusEnum <= 7
+    ),
+    BusinessKeysData
+    AS
+    (
+        SELECT DISTINCT BusinessKey
+        FROM EventsData
     ),
     EventsDataXml
     AS
@@ -34,14 +40,71 @@ WITH
         FROM EventsData
     CROSS APPLY ContentXML.nodes('/eSocial/evtRemun/dmDev/infoPerApur/ideEstabLot/remunPerApur/itensRemun[descFolha]/descFolha') AS t(n)
     ), WorkerCreditData AS (
-        SELECT e.EventId, w.MainEntityCode, e.EntityCode, e.RelatedYear, e.RelatedMonth, e.Matricula, 
-            w.IfConcessoraCodigo C_Financeira, w.Contrato C_Contrato, w.ValorParcela C_Parcela, 
-            e.InstituicaoFinanceira E_Financeira, e.NumeroContrato E_Contrato, e.ValorRubrica E_Parcela,
-            IIF(w.IfConcessoraCodigo = e.InstituicaoFinanceira, 1, 0) ValidaFinanceira,
-            -- IIF(w.Contrato = e.NumeroContrato, 1, 0) ValidaContrato,
-            IIF(w.ValorParcela = e.ValorRubrica, 1, 0) ValidaParcela
-        FROM EventsDataXml e
-        JOIN WorkerCredit w ON w.[Year] = e.RelatedYear AND w.[Month] = e.RelatedMonth AND w.Matricula = e.Matricula AND w.Contrato = e.NumeroContrato
-    )
-SELECT *
-FROM WorkerCreditData
+        SELECT 
+            * 
+        FROM WorkerCredit w
+            LEFT JOIN BusinessKeysData b ON w.Matricula = b.BusinessKey
+        WHERE w.MainEntityCode = @MainEntityCode
+    ), WorkerCreditCompare AS (
+    SELECT 
+        e.EventId, w.MainEntityCode, e.EntityCode, e.RelatedYear, e.RelatedMonth, 
+        e.CPF, e.Matricula,
+        
+        -- Dados para comparação
+        w.IfConcessoraCodigo C_Financeira, w.Contrato C_Contrato, w.ValorParcela C_Parcela,
+        e.InstituicaoFinanceira E_Financeira, e.NumeroContrato E_Contrato, e.ValorRubrica E_Parcela,
+        
+        -- Validações individuais
+        CASE 
+            WHEN w.IfConcessoraCodigo = e.InstituicaoFinanceira THEN 'OK'
+            WHEN w.IfConcessoraCodigo IS NULL THEN 'Consignado sem financeira'
+            WHEN e.InstituicaoFinanceira IS NULL THEN 'eSocial sem financeira'
+            ELSE 'DIVERGENTE'
+        END ValidaFinanceira_Status,
+        
+        CASE 
+            WHEN w.Contrato = e.NumeroContrato THEN 'OK'
+            WHEN w.Contrato IS NULL THEN 'Consignado sem contrato'
+            WHEN e.NumeroContrato IS NULL THEN 'eSocial sem contrato'
+            ELSE 'DIVERGENTE'
+        END ValidaContrato_Status,
+        
+        CASE 
+            WHEN ABS(w.ValorParcela - e.ValorRubrica) <= 0.01 THEN 'OK' -- Tolerância de centavos
+            WHEN w.ValorParcela IS NULL THEN 'Consignado sem valor'
+            WHEN e.ValorRubrica IS NULL THEN 'eSocial sem valor'
+            WHEN w.ValorParcela > e.ValorRubrica THEN 'Valor maior no consignado'
+            WHEN w.ValorParcela < e.ValorRubrica THEN 'Valor maior no eSocial'
+            ELSE 'DIVERGENTE'
+        END ValidaParcela_Status,
+        
+        -- Diferença monetária
+        (e.ValorRubrica - w.ValorParcela) DiferencaValor
+        
+    FROM EventsDataXml e
+    FULL OUTER JOIN WorkerCreditData w ON 
+        w.Matricula = e.Matricula 
+        AND w.Contrato = e.NumeroContrato
+        -- AND w.MainEntityCode = '5111'
+    WHERE w.[Year] = e.RelatedYear 
+        AND w.[Month] = e.RelatedMonth 
+        -- AND w.MainEntityCode = '3151' OR w.MainEntityCode IS NULL
+)
+
+-- Query final para análise
+SELECT 
+    *,
+    CASE 
+        WHEN EventId IS NULL THEN 'Consignado não enviado ao eSocial'
+        WHEN C_Contrato IS NULL THEN 'Desconto no eSocial sem consignado'
+        WHEN ValidaFinanceira_Status = 'OK' AND ValidaContrato_Status = 'OK' AND ValidaParcela_Status = 'OK' 
+            THEN 'CORRETO'
+        ELSE 'CONFERIR'
+    END StatusGeral
+FROM WorkerCreditCompare
+-- WHERE ValidaFinanceira_Status != 'OK' 
+--    OR ValidaContrato_Status != 'OK' 
+--    OR ValidaParcela_Status != 'OK'
+--    OR EventId IS NULL 
+--    OR C_Contrato IS NULL
+ORDER BY Matricula;
